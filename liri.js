3969f569fa1565604499c03d4a3cbe41ab8ddf85
jmm2016 <sus.magallanes@gmail.com>
2017-04-18 23:00:45 -0500

    movie cmd works

--- liri.js
+++ liri.js
@@ -12,56 +12,21 @@ let spotify = require("spotify");
 let client = new Twitter(keys.twitterKeys);
 
 
-inquirer.prompt([
-
-  // Here we create a basic text prompt.
+inquirer.prompt([  
   {
     type: "input",
-    message: "Enter a command?",
+    message: `Enter a command:  Options [ my-tweets, spotify-this-song '<song name here>',  movie-this '<movie name here>', do-what-it-says ]\n `,
     name: "cmd"
-  },
-
-  // Here we create a basic password-protected text prompt.
-  // {
-  //   type: "password",
-  //   message: "Please set your password",
-  //   name: "password"
-  // },
-  //
-  // // Here we give the user a list to choose from.
-  // {
-  //   type: "list",
-  //   message: "Which sport is your favorite?",
-  //   choices: ["Baseball", "BasketBall", "Football", "Hockey", "Soccer"],
-  //   name: "sport"
-  // },
-  //
-  // {
-  // 	type: "checkbox",
-  // 	message: "Did you do any of the following for your favorite sport?",
-  // 	choices: ["Play", "Watch", "Play & Watch"],
-  // 	name: "did"
-  // },
-
-  // Here we ask the user to confirm.
-  {
-    type: "confirm",
-    message: "Are you sure:",
-    name: "confirm",
-    default: true
   }
-
 ]).then(function(answers) {  
-  //console.log(JSON.stringify(answers, null, 2));
-  //console.log("ans.conf: " + answers.confirm);
+   //console.log(JSON.stringify(answers, null, 2));     
   
-  if (answers.confirm) {
-    switch (answers.cmd.toLowerCase()) {
+    let cmd = answers.cmd.toLowerCase();
+    cmd = cmd.split(" ");
+    switch (cmd[0]) {
         case ("t"):
-        case ("my-tweets"):    			
-          //var params = {screen_name: 'miguel_m2017'};
-    			client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=miguel_m2017&count=20', function (error, tweets, response) {
-          //client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        case ("my-tweets"):    			          
+    			client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=miguel_m2017&count=20', function (error, tweets, response) {          
     				if (error) {
     					console.error(error);
     				}
@@ -72,18 +37,32 @@ inquirer.prompt([
               }              
     				}
             else {
-              console.log("sc: " + response.statusCode);
+              console.log("status code: " + response.statusCode);
             }
 
 			    });	
           break;
+        
+        case ("m"):
+        case ("movie-this"):
+          if (cmd.length > 1) {
+            let movie = cmd.slice(1);
+            movie = movie.join(" ");
+            console.log("movie = " + movie);
+            request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json", function(error, response, body) {
+              if (error) {
+                console.error(error);
+              }
+          
+              if (response.statusCode == 200) {                  
+                console.log(body);
+              } 
+            });
+          }  
+          break;
+
         default:
           console.log("no match for cmd: " + answers.cmd);
-    }
-
-  // If the user does not confirm, then a message is provided and the program quits.
-  } else {
-    console.log("Didn't confirm");
-  }
-
+    }  
+    
 });
